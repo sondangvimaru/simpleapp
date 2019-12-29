beb726b65e666cdb647211824ac5f45c9ab1b052
son dang <dangson28121998@gmail.com>
2019-12-29 23:38:37 +0700

    new app

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const express = require('express')
 const fs = require('fs')
 const path = require('path')
 const app = express()
-
+ 
 app.use(express.static(path.join(__dirname, 'public')))
-
+ 
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname + '/index.htm'))
 })
